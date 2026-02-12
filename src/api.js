b3de0af5f84fd841d9a1b4385b62fbcc6f511b62
Mohamed Askhardeen <mohamedaskhardeenbava@gmail.com>
2026-02-12 20:46:50 +0530

    update code

--- src/api.js
+++ src/api.js
@@ -1,18 +1,18 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000",
-  headers: {
-    "Content-Type": "application/json"
-  }
-});
-
-export default api;
-
 // import axios from "axios";
 
 // const api = axios.create({
-//   baseURL: "https://samcafedata.onrender.com"
+//   baseURL: "http://localhost:5000",
+//   headers: {
+//     "Content-Type": "application/json"
+//   }
 // });
 
 // export default api;
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://samcafedata.onrender.com"
+});
+
+export default api;
